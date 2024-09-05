6b3980d2ec8677a9b871164069d53c11ee8a0afd
Magnus <s215773@dtu.dk>
2024-09-05 13:22:19 +0200

    fixed formatting

--- src/protocol.c
+++ src/protocol.c
@@ -1,10 +1,6 @@
 #include "protocol.h"
 
-void protocol_request_to_le(const BEProtocolRequest *be,
-                            LEProtocolRequest *le) {}
-void protocol_response_to_le(const BEProtocolResponse *be,
-                             LEProtocolResponse *le) {}
-void protocol_request_to_be(const LEProtocolRequest *le,
-                            BEProtocolRequest *be) {}
-void protocol_response_to_be(const LEProtocolResponse *le,
-                             BEProtocolResponse *be) {}
+void protocol_request_to_le(const BEProtocolRequest* be, LEProtocolRequest* le) {}
+void protocol_response_to_le(const BEProtocolResponse* be, LEProtocolResponse* le) {}
+void protocol_request_to_be(const LEProtocolRequest* le, BEProtocolRequest* be) {}
+void protocol_response_to_be(const LEProtocolResponse* le, BEProtocolResponse* be) {}
