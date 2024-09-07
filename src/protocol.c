ef8e7f2ce648579ce8063eef024950e57bdab099
Magnus <s215773@dtu.dk>
2024-09-08 01:30:57 +0200

    for real this time (it works)

--- src/protocol.c
+++ src/protocol.c
@@ -1,8 +1,8 @@
 #include "protocol.h"
 
 void protocol_request_to_le(ProtocolRequest *req) {
-    req->start = ntole64(req->start);
-    req->end = ntole64(req->end);
+    req->start = htobe64(req->start);
+    req->end = htobe64(req->end);
 }
 
 void protocol_response_to_be(ProtocolResponse *resp) {
