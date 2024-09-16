d045f9535b10e57640135f4cf1152c6f6ff012a1
Javad Asgari Shafique <me@javad.sh>
2024-09-17 00:53:25 +0200

    Add simple reverse_hash_x4 and benchmarks

--- src/hash.c
+++ src/hash.c
@@ -1,5 +1,4 @@
 #include "hash.h"
-#include "sha256/sha256.h"
 
 uint64_t reverse_hash(uint64_t start, uint64_t end, HashDigest target) {
 
@@ -8,10 +7,36 @@ uint64_t reverse_hash(uint64_t start, uint64_t end, HashDigest target) {
 
         sha256_fullyfused(out, (uint8_t *)&i);
 
-        if (out[0] == target[0] && __builtin_memcmp(out, target, 32) == 0) {
+        if (out[0] == target[0] &&
+            memcmp(out, target, SHA256_DIGEST_LENGTH) == 0) {
             return i;
         }
     }
+
+    return 0;
+}
+
+uint64_t reverse_hash_x4(uint64_t start, uint64_t end, HashDigest target) {
+    uint64_t data[4] = {start, start + 1, start + 2, start + 3};
+    uint8_t hash[SHA256_DIGEST_LENGTH * 4];
+
+    do {
+        sha256x4_fullyfused(hash, (uint8_t *)data);
+
+        // Smartly compare the results
+        for (int i = 0; i < 4; i++) {
+            if (hash[i * SHA256_DIGEST_LENGTH] == target[0] &&
+                memcmp(&hash[i * SHA256_DIGEST_LENGTH], target,
+                       SHA256_DIGEST_LENGTH) == 0) {
+                return data[i];
+            }
+        }
+
+        data[0] += 4;
+        data[1] += 4;
+        data[2] += 4;
+        data[3] += 4;
+    } while (data[0] < end);
 
     return 0;
 }
