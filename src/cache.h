49c00e1c8d95ac0abd0810312d908fa4c3555947
Javad Asgari Shafique <me@javad.sh>
2024-10-11 01:47:45 +0200

    Initial bad restructure

--- src/cache.h
+++ src/cache.h
@@ -8,39 +8,69 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define CACHE_KEY_LENGTH_PACKED (SHA256_DIGEST_LENGTH)
 #define CACHE_KEY_LENGTH (SHA256_DIGEST_LENGTH * 2)
 #define CACHE_KEY_HASH_MAX 16
 
 typedef struct TreeNode TreeNode;
 
-struct TreeNode {
-    union {
-        struct {
-            uint32_t next[CACHE_KEY_HASH_MAX];
-        } branch;
 
-        struct {
-            uint8_t data[CACHE_KEY_LENGTH];
-            uint32_t length;
-            uint32_t next;
-        } edge;
-
-        struct {
-            uint64_t value;
-        } leaf;
-    };
-
-    enum {
-        TT_NONE = 0,
-        TT_BRANCH,
-        TT_EDGE,
-        TT_LEAF,
-    } type;
+enum TreeNodeType
+{
+    TT_NONE = 0,
+    TT_BRANCH = 1,
+    TT_EDGE = 2,
+    TT_LEAF = 3,
 };
 
-struct Cache {
+struct TreeNodePointer
+{
+    uint32_t type : 2;
+    uint32_t idx : 30;
+};
+
+typedef struct TreeNodePointer TreeNodePointer;
+
+struct TreeNodeBranch
+{
+    TreeNodePointer next[CACHE_KEY_HASH_MAX];
+};
+
+struct TreeNodeEdge
+{
+    union
+    {
+        uint8_t data[4];
+        uint32_t str_idx;
+    };
+
+    uint8_t length;
+    TreeNodePointer next;
+};
+
+struct TreeNodeLeaf
+{
+    uint64_t value;
+};
+
+union TreeNodeKeyEntry
+{
+    uint8_t str[CACHE_KEY_LENGTH];
+};
+
+struct Cache
+{
     pthread_mutex_t lock;
-    Vec(TreeNode) nodes;
+
+    TreeNodePointer root;
+
+    FreeList(union TreeNodeKeyEntry) strings;
+
+    FreeList(struct TreeNodeBranch) branches;
+
+    FreeList(struct TreeNodeEdge) edges;
+
+    FreeList(struct TreeNodeLeaf) leaves;
 };
 
 typedef struct Cache Cache;
@@ -48,48 +78,20 @@ typedef struct Cache Cache;
 Cache* cache_create(uint32_t default_cap);
 void cache_destroy(Cache* cache);
 
+
 uint64_t cache_get(Cache* cache, HashDigest key);
 void cache_insert(Cache* cache, HashDigest key, uint64_t value);
 
-static void cache_debug_print_node(const Cache* cache, const TreeNode* node, const int indent) {
-    switch (node->type) {
-    case TT_BRANCH: {
-        printf("%*sBranch\n", indent, "");
-        for (int i = 0; i < CACHE_KEY_HASH_MAX; i++) {
-            if (node->branch.next[i] != 0) {
-                printf("%*s[%x]\n", indent + 2, "", i);
-                cache_debug_print_node(cache, &vec_get(&cache->nodes, node->branch.next[i]), indent + 4);
-            }
-        }
-    } break;
-
-    case TT_EDGE: {
-        printf("%*sEdge\n", indent, "");
-        printf("%*sLength: %d\n", indent + 2, "", node->edge.length);
-        printf("%*sData: ", indent + 2, "");
-        for (int i = 0; i < node->edge.length; i++) {
-            printf("%01x", node->edge.data[i]);
-        }
-        printf("\n");
-        cache_debug_print_node(cache, &vec_get(&cache->nodes, node->edge.next), indent + 2);
-    } break;
-
-    case TT_LEAF: {
-        printf("%*sLeaf\n", indent, "");
-        printf("%*sValue: %lu\n", indent + 2, "", node->leaf.value);
-    } break;
-
-    case TT_NONE: {
-        // Unreachable.
-        printf("%*sNone\n", indent, "");
-    }
-    }
+static void cache_debug_print_node(const Cache* cache, const TreeNode* node, const int indent)
+{
 }
 
 // /*
-inline uint8_t cache_key_hash_idx(const HashDigest hash, const uint32_t idx) {
+inline uint8_t cache_key_hash(const HashDigest hash, const uint32_t idx)
+{
     return (hash[idx >> 1] >> ((~idx & 1) << 2)) & 0xf;
 }
+
 //*/
 
 /*
@@ -98,20 +100,22 @@ inline uint8_t cache_key_hash_idx(const HashDigest key, const uint64_t idx) {
 }
 //*/
 
-static void cache_debug_print(const Cache* cache) {
+static void cache_debug_print(const Cache* cache)
+{
     printf("===============================<CACHE>=================================\n");
-    cache_debug_print_node(cache, &vec_get(&cache->nodes, 0), 0);
 }
 
-static void cache_debug_print_idx(const HashDigest hash) {
-	printf("\t");
-	for (uint32_t i = 0; i < 32; i++) {
-		printf("%02x ", hash[i]);
-	}
-	printf("\n\t");
-	for (uint32_t i = 0; i < 32; i++) {
-		printf("%01x%01x ", cache_key_hash_idx(hash, i * 2), cache_key_hash_idx(hash, i * 2 + 1));
-	}
-	printf("\n");
-
+static void cache_debug_print_idx(const HashDigest hash)
+{
+    printf("\t");
+    for (uint32_t i = 0; i < 32; i++)
+    {
+        printf("%02x ", hash[i]);
+    }
+    printf("\n\t");
+    for (uint32_t i = 0; i < 32; i++)
+    {
+        printf("%01x%01x ", cache_key_hash(hash, i * 2), cache_key_hash(hash, i * 2 + 1));
+    }
+    printf("\n");
 };
