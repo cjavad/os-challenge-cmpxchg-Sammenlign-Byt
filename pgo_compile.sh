86651b4d83a70cc4871fa7a6d2c6f64922e98f10
Javad Asgari Shafique <me@javad.sh>
2024-10-29 22:53:50 +0100

    Fix pgo_compile.sh

--- pgo_compile.sh
+++ pgo_compile.sh
@@ -2,13 +2,13 @@
 
 CC=gcc
 BIN=server_pgo
-CFLAGS="-O3 -flto=auto -march=sandybridge -mtune=sandybridge -masm=intel -std=gnu11 -D_GNU_SOURCE -DRELEASE -lpthread -lrt -lm -Wl,-rpath,lib -I./src"
+CFLAGS="-O3 -flto=auto -march=native -masm=intel -std=gnu11 -D_GNU_SOURCE -DRELEASE -lpthread -lrt -lm -Wl,-rpath,lib -I./src"
 FILES=$(find src -name "*.c")
 
 $CC $CFLAGS $FILES -flto -fprofile-generate -o $BIN
 
 # Run profiling
-./pgo_final benchmark
+./$BIN benchmark
 
 $CC $CFLAGS $FILES -fprofile-use -Werror=missing-profile -o $BIN
 
