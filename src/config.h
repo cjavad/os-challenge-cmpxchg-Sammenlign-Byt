6c2e2d9db78a2b09fff0fc419b5dc908a5e3963d
Javad Asgari Shafique <me@javad.sh>
2024-11-16 17:45:43 +0100

    Default to the fastest baseline (before asm-new branch) with futex waking.

--- src/config.h
+++ src/config.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #ifndef SCHEDULER_WAKER_TYPE
-#define SCHEDULER_WAKER_TYPE 2
+#define SCHEDULER_WAKER_TYPE 1
 #endif
 
 #define HASH_FUNC      sha256_fullyfused
-#define HASH_FUNC_X4   sha256x4_fused
+#define HASH_FUNC_X4   sha256x4_cyclic_asm
 #define HASH_FUNC_X4X2 sha256x4x2_fused
 
-#define REVERSE_FUNC reverse_sha256_x4x2orx8
+#define REVERSE_FUNC reverse_sha256_x4
 
 typedef struct PriorityScheduler SchedulerImpl;
 typedef struct EpollServerCtx ServerImplCtx;
